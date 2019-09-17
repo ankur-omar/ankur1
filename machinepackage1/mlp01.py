01602878ac5ddeed625a5339572d8a07590bb499
xop-er <ankur.omar_cs17@gla.ac.in>
2019-09-17 20:21:54 +0530

    Initial commit

---
+++ machinepackage1/mlp01.py
@@ -0,0 +1,22 @@
+import numpy
+print("numpy:",numpy.__version__)
+
+import matplotlib
+print("matplotlib:",matplotlib.__version__)
+
+import pandas
+print("pandas:",pandas.__version__)
+
+import sklearn
+print("sklearn:",sklearn.__version__)
+
+import scipy
+print("scipy:",scipy.__version__)
+
+import pillow
+print("pillow:",pillow.__version__)
+
+import seaborn
+print("seaborn:",seaborn.__version__)
+
+
